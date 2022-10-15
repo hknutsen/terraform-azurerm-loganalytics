7124c61bf3f3b5ebc47c66871234edcf3d159520
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-10-15 14:34:41 +0200

    chore: add simple example

---
+++ examples/simple-loganalytics/main.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "random_id" "example" {
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-${random_id.example.hex}"
+  location = var.location
+}
+
+module "loganalytics" {
+  # source = "hknutsen/loganalytics/azurerm"
+  # version = "~> 0.0.0"
+  source = "../.."
+
+  workspace_name      = "log-${random_id.example.hex}"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+}
